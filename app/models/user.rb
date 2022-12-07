fb053df64cc83de802d7999d4984b3a63cca5de0
EC2 Default User <ec2-user@ip-172-31-26-84.ap-northeast-1.compute.internal>
2022-12-07 09:47:45 +0000

    応用課題５

--- app/models/user.rb
+++ app/models/user.rb
@@ -36,6 +36,21 @@ def following?(user)
   followings.include?(user)
 end
 
+
+  def self.looks(search, word)
+    if search == "perfect_match"
+      @user = User.where("name LIKE?", "#{word}")
+    elsif search == "forward_match"
+      @user = User.where("name LIKE?","#{word}%")
+    elsif search == "backward_match"
+      @user = User.where("name LIKE?","%#{word}")
+    elsif search == "partial_match"
+      @user = User.where("name LIKE?","%#{word}%")
+    else
+      @user = User.all
+    end
+  end
+
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'searches/search'
   get 'book_comments/create'
   get 'book_comments/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
@@ -18,5 +19,7 @@ Rails.application.routes.draw do
     get 'followers' => 'relationships#followers', as: 'followers'
   end
 
+
+  get 'search' => 'searches#search'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
