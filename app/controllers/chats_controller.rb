db91999983c7703a6e69f2fa36b7c6bac0d3ca77
EC2 Default User <ec2-user@ip-172-31-26-84.ap-northeast-1.compute.internal>
2022-12-11 11:09:49 +0000

    応用課題8a

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,6 +1,7 @@
 class ChatsController < ApplicationController
   before_action :reject_non_related, only: [:show]
 
+
   def show
     @user = User.find(params[:id])
     rooms = current_user.user_rooms.pluck(:room_id)
